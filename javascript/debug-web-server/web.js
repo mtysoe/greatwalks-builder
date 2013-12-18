cff389be2992dd542e2a50fd71ebffb478e68ca8
Matthew Holloway <matthew@holloway.co.nz>
2013-12-18 23:38:28 +1300

    Improving debug

--- javascript/debug-web-server/web.js
+++ javascript/debug-web-server/web.js
@@ -1,17 +1,21 @@
+/*globals require, process, console*/
 // Note: Useful only for debug purposes when using desktop browsers.
 // Chrome doesn't like making geopositioning available when accessing files via file:///
 // so a small web server allows accessing browser features
 
+"use strict";
+
 var http = require("http"),
     url = require("url"),
     path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+    fs = require("fs"),
+    port = process.argv[2] || 8888,
+    webserver;
 
-http.createServer(function(request, response) {
+webserver = http.createServer(function(request, response) {
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+  var uri = url.parse(request.url).pathname,
+      filename = path.join(process.cwd(), uri);
   
   path.exists(filename, function(exists) {
     if(!exists) {
@@ -24,7 +28,7 @@ http.createServer(function(request, response) {
 	if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
@@ -36,6 +40,21 @@ http.createServer(function(request, response) {
       response.end();
     });
   });
-}).listen(parseInt(port, 10));
+});
+
+
+webserver.on('error', function(error){
+  if(error.toString().indexOf("EADDRINUSE") !== -1) {
+    console.log("ERROR: Web Server not started -- address and port are already in use.");
+  } else {
+    throw error;
+  }
+});
+
+try {
+  webserver.listen(parseInt(port, 10));
+} catch(errr) {
+  console.log("what");
+}
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
